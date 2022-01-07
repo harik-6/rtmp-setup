bd777b205ed742f1f26883d29bcd8bb0aa7a5ce8
harik-6 <haribalaji32@gmail.com>
2022-01-07 21:52:45 +0530

    chore:nginx,node version update

--- rtmp-setup/install.sh
+++ rtmp-setup/install.sh
@@ -8,20 +8,19 @@ sudo apt update -y
 sudo apt upgrade -y
 echo "Starting setup process";
 echo "Installing dependancies.";
-sudo apt-get install build-essential libpcre3 libpcre3-dev libssl-dev zlib1g zlib1g-dev nload wondershaper -y
+sudo apt-get install build-essential libpcre3 libpcre3-dev libssl-dev zlib1g zlib1g-dev -y
+sudo apt-get install nload wondershaper
 echo "Intalling dependancie complete.";
 echo "Downloading rtmp module and files.";
+cd ~
 git clone https://github.com/sergey-dryabzhinsky/nginx-rtmp-module.git
-wget http://nginx.org/download/nginx-1.18.0.tar.gz
-curl -o- https://raw.githubusercontent.com/harik-6/rtmpsetup/main/nginx.conf > nginx.conf;
-curl -o- https://raw.githubusercontent.com/harik-6/rtmpsetup/main/nginx.service > nginx.service;
-curl -o- https://raw.githubusercontent.com/harik-6/rtmpsetup/main/stat.xsl > stat.xsl;
+wget http://nginx.org/download/nginx-1.21.5.tar.gz
 echo "${green} Download complete. ${reset}";
-tar -xf nginx-1.18.0.tar.gz
-rm -rf nginx-1.18.0.tar.gz
+tar -xf nginx-1.21.5.tar.gz
+rm -rf nginx-1.21.5.tar.gz
 echo "Unziped and deleted arhive.";
 echo "Configuration rtmp.This might take some time...";
-cd nginx-1.18.0 && ./configure --with-http_ssl_module --add-module=../nginx-rtmp-module
+cd nginx-1.21.5 && ./configure --with-http_ssl_module --add-module=../nginx-rtmp-module
 echo "Configuration rtmp done.OK";
 echo "Running install....."
 sudo make
@@ -30,15 +29,15 @@ cd ..
 echo "Nginx-Rtmp setup complete.";
 echo "Renaming existing conf file and creating new conf"
 mv /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/original.nginx.conf
-mv nginx.conf /usr/local/nginx/conf/;
-mv stat.xsl /usr/local/nginx/html/;
+mv rtmp-setup/nginx.conf /usr/local/nginx/conf/;
+mv rtmp-setup/stat.xsl /usr/local/nginx/html/;
 echo "Creating hls drirectory and adding root access";
 cd /usr/local/nginx/conf && sudo mkdir /nginx
 sudo mkdir /nginx/hls
 sudo chown -R www-data:www-data /nginx
 cd ~
 echo "Adding nginx to startup";
-mv nginx.service /lib/systemd/system/nginx.service
+mv rtmp-setup/nginx.service /lib/systemd/system/nginx.service
 systemctl enable nginx
 systemctl daemon-reload
 echo "${green} Setup completed.OK ${reset}";
@@ -70,23 +69,17 @@ sleep 5s;
 export NVM_DIR="$HOME/.nvm";
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh";
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion";
-echo "Installing node v14.17.5";
-nvm install v14.17.5;
+echo "Installing node v16.13.1";
+nvm install v16.13.1;
 rm nvminstall.sh
 echo "${green} Installing done.OK ${reset}";
-echo "Setting up load job".;
-echo "Downloading git repo.";
-git clone https://github.com/harik-6/rtmpload.git;
-sleep 10s;
-echo "${green} Git repo download success.OK ${reset}";
 echo "Installing node modules.";
-cd rtmpload;
+cd ~/rtmpload;
 npm install;
 echo "${green} Node modules installation done.OK ${reset}";
 echo "Adding pm2 to start up"
 npm install pm2 -g;
 pm2 startup;
 echo "${green} Server setup done.OK ${reset}";
-cd rtmpload && pm2 start load.js --name=loadjob
 echo "=======================================================================================";
 echo "=======================================================================================";
